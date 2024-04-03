bf1aeb5fa28b07015a6066bc4bd1cb7e5991d201
jubaer145 <mdjubaerhossain7@gmail.com>
2024-04-04 02:01:17 +0600

    Change denominator condition,  compute result method and add therapy value sets

--- protocols/PostmenopausalHormoneTherapy.py
+++ protocols/PostmenopausalHormoneTherapy.py
@@ -6,7 +6,7 @@ from canvas_workflow_kit.protocol import (
 )
 
 from canvas_workflow_kit.constants import CHANGE_TYPE
-from canvas_workflow_kit.recommendation import  InstructionRecommendation
+from canvas_workflow_kit.recommendation import  InstructionRecommendation, PrescribeRecommendation
 from canvas_workflow_kit.value_set import ValueSet
 
 
@@ -17,10 +17,27 @@ class PostmenopausalState(ValueSet):
 class Hysterectomy(ValueSet):
     VALUE_SET_NAME = 'Hysterectomy'
     ICD10CM = {'Z90.710'}
+    SNOMEDCT = {'236886002'}
 
 class PostmenopausalHormoneTherapyInstruction(ValueSet):
     VALUE_SET_NAME = 'Postmenopausal hormone replacement therapy'
     ICD10CM = {'Z79.890'}
+
+class EstrogenTherapy(ValueSet):
+    VALUE_SET_NAME = 'Estrogen therapy'
+    LOINC = {'2254-1'}
+
+
+class ProgestinTherapy(ValueSet):
+    VALUE_SET_NAME = 'Progestin therapy'
+    LOINC = {'2839-9'}
+    
+class EstrogenAndProgestinTherapy(ValueSet):
+    VALUE_SET_NAME = 'Progestin therapy'
+    LOINC = {'2839-9', '2254-1'}
+    
+    
+
 
 class HormoneTherapyProtocol(ClinicalQualityMeasure):
 
@@ -51,37 +68,64 @@ class HormoneTherapyProtocol(ClinicalQualityMeasure):
 
     def in_denominator(self):
         """
-        Patients in the postmenopausal state.
+        If patient is in the postmenopausal state.
         """
-        return self.patient.conditions.find(PostmenopausalState)
+        return len(self.patient.conditions.find(PostmenopausalState)) > 0
 
     def in_numerator(self):
         """
-        Postmenopausal patient who had hysterectomy.
+        Menopausal hormone therapy refers to the use of combined estrogen and progestin in persons with an intact uterus, or estrogen
+        alone in persons who have had a hysterectomy, taken at or after
+        the time of menopause.
         """
-        return self.patient.procedures.find(Hysterectomy)
+        progestin_therapy = len(self.patient.medications.find(ProgestinTherapy)) > 0
+        estrogen_therapy = len(self.patient.medications.find(EstrogenTherapy)) > 0    
+        
+        # if patient has hysterectomy
+        if len(self.patient.conditions.find(Hysterectomy)) > 0:
+            # patient should have only estrogen therapy
+            return estrogen_therapy and not progestin_therapy
+        # otherwise patient should have both estrogen and progestin therapy
+        return estrogen_therapy and progestin_therapy
+            
+     
+     
+     
+            
 
     def compute_results(self):
         result = ProtocolResult()
 
-        if self.in_denominator(): # ifpatient is in postmenopausal state
-            if self.in_numerator(): # if patient also had hysterectomy
-                result.status = STATUS_SATISFIED
-                result.add_narrative(
-                    f'{self.patient.first_name} has been given recommendations.'
-                )
-
-                result.add_recommendation(
-                    InstructionRecommendation(
-                        key='RECOMMEND_DISCUSS_HORMONE_INTAKE',
-                        rank=1,
-                        button='Instruct',
-                        patient=self.patient,
-                        instruction=PostmenopausalHormoneTherapyInstruction,
-                        title='Discuss ans Instruct on Hormone Intake in Postmenopausal State'
-                        
-                        
-                        
+        if self.in_denominator(): # if patient is in postmenopausal state
+            if len(self.patient.conditions.find(Hysterectomy)) > 0: # if patient had hysterectomy 
+                # if patient had hysterectomy but not having only estrogen therapy   
+                if not self.in_numerator():
+                    result.status = STATUS_DUE
+                    result.add_recommendation(
+                        PrescribeRecommendation(
+                            key='RECOMMEND_ESTROGEN_THERAPY',
+                                    rank=1,
+                                    button='Prescribe',
+                                    patient=self.patient,
+                                    prescription=EstrogenTherapy,
+                                    title='Recommendation of Estrogen Therapy.',
+                                    context={}
+                                )
                     )
-                )
+            else: # if patient did not have hysterectomy   
+                # patient not having both estrogen and progestin therapy
+                if not self.in_numerator():      
+                    result.status = STATUS_DUE
+                    result.add_recommendation(
+                        PrescribeRecommendation(
+                            key='RECOMMEND_ESTROGEN_AND_PROGESTIN_THERAPY',
+                                    rank=1,
+                                    button='Prescribe',
+                                    patient=self.patient,
+                                    prescription=EstrogenAndProgestinTherapy,
+                                    title='Recommendation of Estrogen and Progestin Therapy.',
+                                    context={}
+                                )
+                    )
+                
         return result
